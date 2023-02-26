c375155bef0487015e1af3d16c7f9c62ec6c7146
Luis Ibargüen <luis.ibargueen@gmx.net>
2023-02-26 22:28:52 +0000

    LLVM preview

--- src/menubutton.h
+++ src/menubutton.h
@@ -4,25 +4,26 @@
 #include <SDL2/SDL.h>
 #include <string>
 
-class MenuButton final
-{
+class MenuButton final {
 public:
-    MenuButton(uint32_t x, uint32_t y, const std::string &name, const SDL_Renderer *renderer);
-    MenuButton(const MenuButton&) = delete;
-    MenuButton & operator= ( const MenuButton & ) = delete;
-    MenuButton(MenuButton&& src) noexcept;
-    MenuButton & operator= ( MenuButton && ) = delete;
-    ~MenuButton();
-    void Render(const SDL_Renderer *renderer) const;
-    void Activate();
-    void Deactivate();
+  MenuButton(uint32_t x, uint32_t y, const std::string &name,
+             const SDL_Renderer *renderer);
+  MenuButton(const MenuButton &) = delete;
+  MenuButton &operator=(const MenuButton &) = delete;
+  MenuButton(MenuButton &&src) noexcept;
+  MenuButton &operator=(MenuButton &&) = delete;
+  ~MenuButton();
+  void Render(const SDL_Renderer *renderer) const;
+  void Activate();
+  void Deactivate();
+
 private:
-    bool isActive;
-    SDL_Texture *icon;
-    SDL_Rect icon_rect;
-    SDL_Texture *backgroundActive;
-    SDL_Texture *background;
-    SDL_Rect rect;
+  bool isActive;
+  SDL_Texture *icon;
+  SDL_Rect icon_rect;
+  SDL_Texture *backgroundActive;
+  SDL_Texture *background;
+  SDL_Rect rect;
 };
 
 #endif // MENUBUTTON_H
